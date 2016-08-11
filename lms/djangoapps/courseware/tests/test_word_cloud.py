1c7794aebf12ed9928217bed4db99cc96ba79cb2
Chris Rodriguez <github@chrisrodriguez.me>
2016-08-11 12:03:16 -0400

    WIP

--- lms/djangoapps/courseware/tests/test_word_cloud.py
+++ lms/djangoapps/courseware/tests/test_word_cloud.py
@@ -246,7 +246,7 @@ class TestWordCloud(BaseTestXmodule):
 
         expected_context = {
             'ajax_url': self.item_descriptor.xmodule_runtime.ajax_url,
-            'display_name': 'word_cloud',
+            'display_name': 'word_cloud ' + self.item_descriptor.location.html_id(),
             'element_class': self.item_descriptor.location.category,
             'element_id': self.item_descriptor.location.html_id(),
             'instructions': 'Replace this text with instructions for your word cloud.',
